2bc01d981a682722ce74f051d4bb0d97e207d949
kayna guarany <kayna@guarany.net>
2017-03-24 14:48:38 -0300

    adiciona migrations

--- db/migrate/20170324134734_create_restaurantes.rb
+++ db/migrate/20170324134734_create_restaurantes.rb
@@ -1,8 +1,9 @@
 class CreateRestaurantes < ActiveRecord::Migration
   def change
     create_table :restaurantes do |t|
-
-      t.timestamps null: false
+        t.string :nome, limit: 80
+        t.string :endereco
+        t.timestamps null: false
     end
   end
 end
